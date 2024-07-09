c55901eeeae84913799dd03aae10e826b221f355
Thames <chiratt.thames@gmail.com>
2024-07-09 15:20:59 +0700

    Fixed responsive display for leaderboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"gold" : "#C8A263",
+				"light-gold" : "#DAC7A6",
+				"brown" : "#544122"
+			},
+		},
 	},
 	plugins: [],
 }
