949f9f9aea347c79b5c50d4b62aeef8ffecd2d2f
Thames <chiratt.thames@gmail.com>
2024-07-12 21:53:59 +0700

    Updated Rules page
    • Updated rules page for mobile
    • Used prettier to format every page
    
    TODO:
    • Work on desktop version of rules
    • Work on Scenario a bit more

--- src/scripts/countdown.js
+++ src/scripts/countdown.js
@@ -1,16 +1,17 @@
 //First reveal: Jul 28, 2024 12:00:00
 //Second reveal: Auditorium time
 var countDownDate = new Date("Jul 28, 2024 12:00:00").getTime();
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("countdown_timer").innerHTML = hours + ":"+ minutes + ":" + seconds;
+var x = setInterval(function () {
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	document.getElementById("countdown_timer").innerHTML =
+		hours + ":" + minutes + ":" + seconds;
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown_timer").innerHTML = "Card is revealed!";
-    }
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("countdown_timer").innerHTML = "Card is revealed!";
+	}
 }, 1000);
