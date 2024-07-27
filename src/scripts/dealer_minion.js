8c4f0c9f0db32deee481172b4619ae8aad66c7b0
Thames <chiratt.thames@gmail.com>
2024-07-28 00:21:34 +0700

    feat: dealers can now change cards

--- src/scripts/dealer_minion.js
+++ src/scripts/dealer_minion.js
@@ -1,5 +1,8 @@
 import { readDB } from "./read.js";
 import { groupNameList } from "./data.js";
+import { db } from "../firebase.js";
+import { collection, doc, getDoc, updateDoc } from "firebase/firestore";
+import GroupInfo from "../components/Group_info.astro";
 
 // Changes Current Info tabs in dealer minion
 document
@@ -12,9 +15,51 @@ document
 				document.getElementById("currentName").innerHTML = cardInfo[1];
 				document.getElementById("currentNumber").innerHTML =
 					cardInfo[0] + " cards";
-				console.log(cardInfo);
+				// console.log(cardInfo);
 			} catch (error) {
 				console.error("Error fetching card info:", error);
 			}
 		}
 	});
+
+// Updates card number in the database
+document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("forms").addEventListener("submit", async (event) => {
+		event.preventDefault();
+
+		const password = document.getElementById("password").value;
+		const groupName = document.getElementById("groupName").value;
+		const number = parseInt(document.getElementById("cardNumber").value, 10);
+
+		try {
+			// Check password
+			const passwordDocRef = doc(db, "Dealer", "passwordDoc");
+			const passwordDoc = await getDoc(passwordDocRef);
+
+			if (passwordDoc.exists()) {
+				const storedPassword = passwordDoc.data().password;
+				// console.log("Stored password:", storedPassword);
+			} else {
+				console.error("No such document!");
+			}
+
+			const storedPassword = passwordDoc.data().password;
+			// console.log(storedPassword);
+			// console.log(password);
+			if (password !== storedPassword) {
+				console.error("Incorrect password!");
+				return;
+			}
+
+			await updateDoc(doc(collection(db, "Groups"), groupName), {
+				//name: groupName,
+				cardNumber: number,
+			});
+
+			console.log("Card number updated successfully!");
+			console.log(number);
+		} catch (error) {
+			console.error("Error:", error);
+		}
+	});
+});
